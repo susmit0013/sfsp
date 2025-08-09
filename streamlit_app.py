03f86011ee73fbc3f5ef829d6b44ba36c3b2f039
Susmit Paul <susmit0013@gmail.com>
2025-08-09 18:51:50 +0530

    Update streamlit_app.py
    sf df response dataframe addition

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,8 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+#st.text(smoothiefroot_response.json())
+sf_df=st.dataframe(data.smoothiefroot_response.json(),use_container_width=True)
 
 #-## Get the current credentials
 #-#session = get_active_session()
